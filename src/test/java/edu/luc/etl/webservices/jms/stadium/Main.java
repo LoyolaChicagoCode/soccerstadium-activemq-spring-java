1244f258c915958da00ca1bc654df5fec8b02bbd
Konstantin Laufer <laufer@cs.luc.edu>
2011-04-11 22:39:39 -0500

    Made Main class more flexible by looking up door instances by type.

--- src/test/java/edu/luc/etl/webservices/jms/stadium/Main.java
+++ src/test/java/edu/luc/etl/webservices/jms/stadium/Main.java
@@ -9,38 +9,34 @@ public class Main {
 
     public static void main(final String... args) throws Exception {
 
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "spring-context.xml");
+        ClassPathXmlApplicationContext context = 
+            new ClassPathXmlApplicationContext("spring-context.xml");
 
         ((Display) context.getBean("display")).start();
         ((Counter) context.getBean("counter")).start();
 
-        final Door[] doors = new Door[] {
-    		((Door) context.getBean("door_north")),
-    		((Door) context.getBean("door_south"))
-        };
+        final Door[] doors = (Door[]) context.getBeansOfType(Door.class).values().toArray(new Door[0]);
         for (final Door d : doors) {
-        	d.start();
+            d.start();
         }
 
         final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("action [enter | leave num]> ");
         String input;
         while ((input = in.readLine()) != null) {
-        	try {
-	        	input = input.trim();
-	            final String action = input.substring(0, 5);
-	            final int num = Integer.parseInt(input.substring(5).trim());
-	            if ("enter".equals(action)) {
-	            	doors[num].enter();
-	            } else if ("leave".equals(action)) {
-	            	doors[num].leave();
-	            }
-        	} catch (final NumberFormatException ex1) {
-        	} catch (final IndexOutOfBoundsException ex2) {
-        	}
+            try {
+                input = input.trim();
+                final String action = input.substring(0, 5);
+                final int num = Integer.parseInt(input.substring(5).trim());
+                if ("enter".equals(action)) {
+                    doors[num].enter();
+                } else if ("leave".equals(action)) {
+                    doors[num].leave();
+                }
+            } catch (final NumberFormatException ex1) {
+            } catch (final IndexOutOfBoundsException ex2) {
+            }
             System.out.print("action [enter | leave num]> ");
         }
-
     }
 }
